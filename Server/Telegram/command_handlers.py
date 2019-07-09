3df0510e9ef75017a3dce90c4cc26b0b5b2bdf23
CuriousGeorgiy <32464072+CuriousGeorgiy@users.noreply.github.com>
2019-07-10 00:17:26 +0300

    Delete command_handlers.py

--- Server/Telegram/command_handlers.py
+++
@@ -1,6 +0,0 @@
-def start(update, context):
-    context.bot.send_message(chat_id=update.message.chat_id, text="Приветствую вас!")
-
-
-def unknown(update, context):
-    context.bot.send_message(chat_id=update.message.chat_id, text="Сожалею, но я не понимаю эту команду.")
