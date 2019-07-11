fa3758ec128d299825b2ce8c49de1e906b6a6a6e
CuriousGeorgiy <32464072+CuriousGeorgiy@users.noreply.github.com>
2019-07-11 18:02:52 +0300

    [google_sheets_api.py]
    refactoring:
    -implemented GoogleSheetsAPI wrapper class around the Google Sheets API with main operations for data fetching

--- resources/google_sheets_api.py
+++ resources/google_sheets_api.py
@@ -1,53 +1,52 @@
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
+from googleapiclient.discovery import build
 
 import os.path
 import pickle
 
 import datetime
 
-SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly']
+class GoogleSheetsAPI:
 
+    def __init__(self, sheets_ids):
+        self._SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly']
+        self._sheets_ids = sheets_ids
 
-def authorization():
-    credentials = None
+        self._authorization()
 
-    if os.path.exists('misc/google_sheets_api_token.pickle'):
-        with open('misc/google_sheets_api_token.pickle', 'rb') as token:
-            credentials = pickle.load(token)
+        self._service = build('sheets', 'v4', credentials=self._credentials)
 
-    if not credentials or not credentials.valid:
-        if credentials and credentials.expired and credentials.refresh_token:
-            credentials.refresh(Request())
-        else:
-            flow = InstalledAppFlow.from_client_secrets_file(
-                'misc/google_sheets_api_credentials.json', SCOPES)
-            credentials = flow.run_local_server()
+    def _authorization(self):
+        self._credentials = None
 
-        with open('misc/google_sheets_api_token.pickle', 'wb') as token:
-            pickle.dump(credentials, token)
+        if os.path.exists('misc/google_sheets_api_token.pickle'):
+            with open('misc/google_sheets_api_token.pickle', 'rb') as token:
+                self._credentials = pickle.load(token)
 
-    return credentials
+        if not self._credentials or not self._credentials.valid:
+            if self._credentials and self._credentials.expired and self._credentials.refresh_token:
+                self._credentials.refresh(Request())
+            else:
+                flow = InstalledAppFlow.from_client_secrets_file('misc/google_sheets_api_credentials.json', self._SCOPES)
+                self._credentials = flow.run_local_server()
 
+            with open('misc/google_sheets_api_token.pickle', 'wb') as token:
+                pickle.dump(self._credentials, token)
 
-def parse_date_time(date_time):
-    date, time = date_time.split()
+    def get_values(self, sheet_id, range):
+        return self.service.spreadsheets().values().get(spreadsheetId=sheet_id, range=range).execute().get('values', [])
 
-    return datetime.datetime(*list(map(int, date.split('.')))[::-1], *list(map(int, time.split(':'))))
+    def extract_faq_sheet(self):
+        return self.get_values(self._sheets_ids['FAQ'], 'A2:B')
 
+    def extract_pushes_sheet(self):
+        def convert_table_date_time(row):
+            row[2] = datetime.datetime.strptime(row[2], '%d.%m.%Y %H:%M:%S')
 
-def get_values(service, table_id, range):
-    return service.spreadsheets().values().get(spreadsheetId=table_id, range=range).execute().get('values', [])
+            return row
 
+        return list(map(convert_table_date_time, self.get_values(self._sheets_ids['PUSHES'], 'A2:C')))
 
-def extract_faq_table(service, faq_table_id):
-    return get_values(service, faq_table_id, 'A2:B')
-
-
-def extract_pushes_table(service, faq_table_id):
-    def convert_table_date_time(row):
-        row[2] = parse_date_time(row[2])
-
-        return row
-
-    return list(map(convert_table_date_time, get_values(service, faq_table_id, 'A2:C')))
+    def extract_all_sheets(self):
+        return self.extract_faq_sheet(), self.extract_pushes_sheet()
