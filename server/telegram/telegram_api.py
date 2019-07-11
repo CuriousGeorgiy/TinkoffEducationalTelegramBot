7a12942d7bd60c1215cda1ab83d1354b762ce1f0
CuriousGeorgiy <32464072+CuriousGeorgiy@users.noreply.github.com>
2019-07-11 18:01:41 +0300

    [telegram_api.py]
    changed callbacks usage

--- server/telegram/telegram_api.py
+++ server/telegram/telegram_api.py
@@ -1,8 +1,6 @@
 import telegram.ext
 
-import server.telegram.command_callbacks
-import server.telegram.message_callbacks
-import server.telegram.push_callbacks
+from server.telegram import callbacks
 
 import util.exception
 
@@ -14,10 +12,9 @@ class TelegramAPI:
         self._faq_list = faq_list
         self._pushes_list = pushes_list
 
-        self._updater.dispatcher.add_handler(telegram.ext.CommandHandler('start',
-                                                                         server.telegram.command_callbacks.start))
+        self._updater.dispatcher.add_handler(telegram.ext.CommandHandler('start', callbacks.command_callbacks.start))
         self._updater.dispatcher.add_handler(
-            telegram.ext.MessageHandler(telegram.ext.Filters.command, server.telegram.command_callbacks.unknown))
+            telegram.ext.MessageHandler(telegram.ext.Filters.command, callbacks.command_callbacks.unknown))
 
         self._add_faq_handlers()
         self._create_push_jobs()
@@ -31,10 +28,15 @@ class TelegramAPI:
     def _add_faq_handlers(self):
         for question, answer in self._faq_list:
             self._updater.dispatcher.add_handler(telegram.ext.MessageHandler(telegram.ext.Filters.regex(question),
-                                                                             server.telegram.message_callbacks.
+                                                                             callbacks.message_callbacks.
                                                                              create_callback_from_answer(answer)))
 
     def _create_push_jobs(self):
         for text, groups, send_time in self._pushes_list:
-            self._updater.job_queue.run_once(server.telegram.push_callbacks.create_callback_from_answer(text),
-                                             send_time)
+            self._updater.job_queue.run_once(callbacks.push_callbacks.create_callback_from_answer(text), send_time)
+
+    def update_faq_list(self, faq_list):
+        self._faq_list = faq_list
+
+    def update_pushes_list(self, pushes_list):
+        self._pushes_list = pushes_list
