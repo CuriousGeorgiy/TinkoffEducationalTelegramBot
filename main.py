f1d85d7f0b6e927c27a54323d881a876c3f146dc
CuriousGeorgiy <32464072+CuriousGeorgiy@users.noreply.github.com>
2019-07-11 18:03:15 +0300

    [main.py]

--- main.py
+++ main.py
@@ -1,30 +1,26 @@
 from server.telegram.telegram_api import TelegramAPI
 
-import googleapiclient.discovery
-
 import logging.config
 
 import util.file_processing
 
 import resources.google_sheets_api
 
+import schedule
+
 
 def main():
-    google_sheets_service = googleapiclient.discovery.build('sheets', 'v4',
-                                                            credentials=resources.google_sheets_api.authorization())
-
-    table_ids = util.file_processing.load_json_from_file('misc/table_ids.json')
+    google_sheets_ids = util.file_processing.load_json_from_file('misc/google_sheets_ids.json')
     logging.config.dictConfig(util.file_processing.load_json_from_file('misc/logging_config.json'))
     authorization_tokens = util.file_processing.load_json_from_file('misc/authorization_tokens.json')
 
-    telegram_interface = TelegramAPI(resources.google_sheets_api.extract_faq_table(google_sheets_service,
-                                                                                   table_ids['FAQ']),
-                                     resources.google_sheets_api.extract_pushes_table(google_sheets_service,
-                                                                                      table_ids['PUSHES']),
-                                     authorization_tokens['telegram_bot_token'])
-    telegram_interface.invoke()
+    google_sheets_api = resources.google_sheets_api.GoogleSheetsAPI(google_sheets_ids)
 
-    print(resources.google_sheets_api.extract_pushes_table(google_sheets_service, table_ids['PUSHES']))
+    telegram_api = TelegramAPI(google_sheets_api.extract_faq_table(), google_sheets_api.extract_pushes_table(),
+                               authorization_tokens['telegram_bot_token'])
+    telegram_api.invoke()
+
+    schedule.run_pending()
 
 
 if __name__ == "__main__":
