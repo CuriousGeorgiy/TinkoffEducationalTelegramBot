a352fd3f50342129cc29c21d2958d726dfc04b9c
CuriousGeorgiy <32464072+CuriousGeorgiy@users.noreply.github.com>
2019-07-15 16:32:51 +0300

    [message_callbacks.py]
    use send_action decorator

--- server/telegram/callbacks/message_callbacks.py
+++ server/telegram/callbacks/message_callbacks.py
@@ -1,4 +1,10 @@
+import server.telegram.util.decorators
+
+from telegram.chataction import ChatAction
+
+
 def create_callback_from_answer(answer):
+    @server.telegram.util.decorators.send_action(ChatAction.TYPING)
     def callback(update, context):
         context.bot.send_message(chat_id=update.message.chat_id, text=answer)
 
