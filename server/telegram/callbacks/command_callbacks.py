da120a1c1270ac53701e293fafe69a94aaee391e
CuriousGeorgiy <32464072+CuriousGeorgiy@users.noreply.github.com>
2019-07-15 16:32:26 +0300

    [command_callbacks.py]
    use send_action decorators

--- server/telegram/callbacks/command_callbacks.py
+++ server/telegram/callbacks/command_callbacks.py
@@ -1,6 +1,13 @@
+import server.telegram.util.decorators
+
+from telegram.chataction import ChatAction
+
+
+@server.telegram.util.decorators.send_action(ChatAction.TYPING)
 def start(update, context):
     context.bot.send_message(chat_id=update.message.chat_id, text="Приветствую вас!")
 
 
+@server.telegram.util.decorators.send_action(ChatAction.TYPING)
 def unknown(update, context):
     context.bot.send_message(chat_id=update.message.chat_id, text="Сожалею, но я не понимаю эту команду.")
