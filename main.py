6c928e44744a41a60f76c681ddb6aa0e93e90e3d
CuriousGeorgiy <32464072+CuriousGeorgiy@users.noreply.github.com>
2019-07-18 21:55:12 +0300

    [main.py]
    changed variable name, now passing proxy url to TelegramAPI class

--- main.py
+++ main.py
@@ -12,11 +12,12 @@ import resources.google_sheets_api
 def main():
     google_sheets_ids = util.file_processing.load_json_from_file('misc/google_sheets_ids.json')
     logging.config.dictConfig(util.file_processing.load_json_from_file('misc/logging_config.json'))
-    authorization_tokens = util.file_processing.load_json_from_file('misc/authorization_tokens.json')
+    telegram_api_config = util.file_processing.load_json_from_file('misc/telegram_api_config.json')
 
     google_sheets_api = resources.google_sheets_api.GoogleSheetsAPI(google_sheets_ids)
 
-    telegram_api = TelegramAPI(*google_sheets_api.extract_all_sheets(), authorization_tokens['telegram_bot_token'])
+    telegram_api = TelegramAPI(*google_sheets_api.extract_all_sheets(), telegram_api_config['bot_token'],
+                               telegram_api_config['proxy_url'])
 
     TelegramUtil.attach_telegram_api(telegram_api)
     TelegramUtil.attach_google_sheets_api(google_sheets_api)
