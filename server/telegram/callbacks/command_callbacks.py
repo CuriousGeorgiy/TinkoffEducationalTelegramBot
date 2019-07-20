a0a4230e9bc3a765b8e9ba1ca84f9cb84d251f70
CuriousGeorgiy <32464072+curiousgeorgiy@users.noreply.github.com>
2019-07-20 13:13:27 +0300

    [command_callbacks.py]
    changes:
    - import telegram library
    - ask for contacts in the start command callback in order to authorize the user
    
    Signed-off-by: CuriousGeorgiy <32464072+curiousgeorgiy@users.noreply.github.com>

--- server/telegram/callbacks/command_callbacks.py
+++ server/telegram/callbacks/command_callbacks.py
@@ -2,10 +2,15 @@ import server.telegram.util.decorators
 
 from telegram.chataction import ChatAction
 
+import telegram
+
 
 @server.telegram.util.decorators.send_action(ChatAction.TYPING)
 def start(update, context):
-    context.bot.send_message(chat_id=update.message.chat_id, text="Приветствую вас!")
+    context.bot.send_message(chat_id=update.message.chat_id, text="Приветствую вас! Для использования бота необходимо "
+                                                                  "авторизоваться по номеру телефона.", reply_markup=
+                             telegram.ReplyKeyboardMarkup([[telegram.KeyboardButton(text="Отправить контакты",
+                                                                                    request_contact=True)]]))
 
 
 @server.telegram.util.decorators.send_action(ChatAction.TYPING)
