ee3af3095724886a09bbde486f162f82d73e5d2c
CuriousGeorgiy <32464072+curiousgeorgiy@users.noreply.github.com>
2019-07-20 13:34:45 +0300

    [message_callbacks.py]
    changes:
    - import telegram library
    - authorization callback function: add a reply markup removing the "Send contacts" button
    
    Signed-off-by: CuriousGeorgiy <32464072+curiousgeorgiy@users.noreply.github.com>

--- server/telegram/callbacks/message_callbacks.py
+++ server/telegram/callbacks/message_callbacks.py
@@ -2,6 +2,8 @@ import server.telegram.util.decorators
 
 from telegram.chataction import ChatAction
 
+import telegram
+
 
 def create_callback_from_answer(answer):
     @server.telegram.util.decorators.send_action(ChatAction.TYPING)
@@ -14,4 +16,5 @@ def create_callback_from_answer(answer):
 @server.telegram.util.decorators.send_action(ChatAction.TYPING)
 def authorization(update, context):
     context.bot.send_message(chat_id=update.message.chat_id, text='Ваш номер телефона: ' +
-                                                                  update.message.contact.phone_number)
+                                                                  update.message.contact.phone_number,
+                             reply_markup=telegram.ReplyKeyboardRemove())
