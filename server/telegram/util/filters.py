4268dda07b9be0024e7bb10bbdea8ed55a48893f
CuriousGeorgiy <32464072+curiousgeorgiy@users.noreply.github.com>
2019-07-20 14:54:05 +0300

    [filters.py]
    changed:
    - a Message object should be passed to regex's filter method instead of a str
    
    Signed-off-by: CuriousGeorgiy <32464072+curiousgeorgiy@users.noreply.github.com>

--- server/telegram/util/filters.py
+++ server/telegram/util/filters.py
@@ -4,9 +4,9 @@ import telegram.ext
 class PrepareMessageTextForRegexFilter(telegram.ext.BaseFilter):
 
     def __init__(self, pattern):
-        super.__init__()
-
         self._pattern = pattern
 
     def filter(self, message):
-        return telegram.ext.Filters.regex(self._pattern).filter(message.text.lower())
+        message.text = message.text.lower()
+
+        return telegram.ext.Filters.regex(self._pattern).filter(message)
