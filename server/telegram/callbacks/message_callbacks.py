0abc9884c5d4b99fc46e9e7a783c3ea7284fe400
CuriousGeorgiy <32464072+curiousgeorgiy@users.noreply.github.com>
2019-07-21 14:55:33 +0300

    [message_callbacks.py]
    changes:
    - add try-except block in the callback function in create_callback_from_answer function in order to catch users that never authorized or if the persistence object data is ever lost
    - add in_telegram_api key to context.user_data
    - dump telegram api persistence object in the end of the authorization callback
    
    Signed-off-by: CuriousGeorgiy <32464072+curiousgeorgiy@users.noreply.github.com>

--- server/telegram/callbacks/message_callbacks.py
+++ server/telegram/callbacks/message_callbacks.py
@@ -8,9 +8,15 @@ def create_callback_from_answer(answer):
     @telegram.ext.dispatcher.run_async
     @server.telegram.decorators.send_action(telegram.ChatAction.TYPING)
     def callback(update, context):
-        if context.user_data['authorized']:
-            context.bot.send_message(chat_id=update.message.chat_id, text=answer)
-        else:
+        try:
+            if context.user_data['authorized']:
+                context.bot.send_message(chat_id=update.message.chat_id, text=answer)
+            else:
+                context.bot.send_message(chat_id=update.message.chat_id, text='Для использования бота необходимо пройти'
+                                                                              ' авторизацию с помощью команды'
+                                                                              ' /authorization.')
+        except KeyError:
+            context.user_data['authorized'] = False
             context.bot.send_message(chat_id=update.message.chat_id, text='Для использования бота необходимо пройти'
                                                                           ' авторизацию с помощью команды'
                                                                           ' /authorization.')
@@ -26,18 +32,18 @@ def authorization(update, context):
 
     if update.message.contact.phone_number in phone_number_to_id:
         context.user_data['authorized'] = True
+        context.user_data['id_in_telegram_api'] = phone_number_to_id[update.message.contact.phone_number]
         TelegramUtil.set_telegram_api_person_telegram_id(phone_number_to_id[update.message.contact.phone_number],
                                                          update.message.chat_id)
-        context.bot.send_message(chat_id=update.message.chat_id, text=context.user_data['info'][0]+', Вы прошли ' 
-                                                                                                   'авторизацию, теперь'
-                                                                                                   ' вы можете' 
-                                                                                                   ' пользоваться'
-                                                                                                   ' ботом.',
+        context.bot.send_message(chat_id=update.message.chat_id, text='Вы прошли авторизацию, теперь вы можете' 
+                                                                      ' пользоваться ботом.',
                                  reply_markup=telegram.ReplyKeyboardRemove())
     else:
         context.user_data['authorized'] = False
         context.bot.send_message(chat_id=update.message.chat_id, text='Сожалею, но Вы не прошли авторизацию.',
                                  reply_markup=telegram.ReplyKeyboardRemove())
+
+    TelegramUtil.dump_telegram_api_persistence_obj()
 
 
 @telegram.ext.dispatcher.run_async
