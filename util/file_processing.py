06a25cdaf8856f4cb59e346dd7c6f310c71ac44f
CuriousGeorgiy <32464072+curiousgeorgiy@users.noreply.github.com>
2019-07-21 12:19:05 +0300

    [file_processing.py]
    implemented:
    - load_json_string_from_file function for work with telegram_api's json persistence
    - dump_json_string_to_file function for work with telegram_api's json persistence
    
    Signed-off-by: CuriousGeorgiy <32464072+curiousgeorgiy@users.noreply.github.com>

--- util/file_processing.py
+++ util/file_processing.py
@@ -4,3 +4,12 @@ import json
 def load_json_from_file(path):
     with open(path, 'r') as file:
         return json.load(file)
+
+
+def load_json_string_from_file(path):
+    return json.dumps(load_json_from_file(path))
+
+
+def dump_json_string_to_file(json_string, path):
+    with open(path, 'w') as file:
+        json.dump(json.loads(json_string), file)
