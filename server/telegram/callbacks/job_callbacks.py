626a3f3067b14758001450fa57759a6ba35f1683
CuriousGeorgiy <32464072+curiousgeorgiy@users.noreply.github.com>
2019-07-21 14:51:48 +0300

    [job_callbacks.py]
    changes:
    - removed send_action decorator causing error as it is used for functions with foor(update, context) notation
    
    Signed-off-by: CuriousGeorgiy <32464072+curiousgeorgiy@users.noreply.github.com>

--- server/telegram/callbacks/job_callbacks.py
+++ server/telegram/callbacks/job_callbacks.py
@@ -6,11 +6,13 @@ import telegram.ext
 
 def create_push_notification_callback_from_push_text(push_text):
     @telegram.ext.dispatcher.run_async
-    @server.telegram.decorators.send_action(telegram.ChatAction.TYPING)
     def callback(context):
         for person in context.job.context[0].values():
             if person['groups'] & context.job.context[1]:
-                context.bot.send_message(chat_id=person['telegram_id'], text=push_text)
+                try:
+                    context.bot.send_message(chat_id=person['telegram_id'], text=push_text)
+                except KeyError:
+                    pass
 
     return callback
 
