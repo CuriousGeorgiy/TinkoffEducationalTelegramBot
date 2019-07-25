c002de25f7d4d48cebf4a867a308cb7bca433e25
CuriousGeorgiy <32464072+curiousgeorgiy@users.noreply.github.com>
2019-07-26 02:23:58 +0300

    [telegram_api.py]
    changes:
    - removed redundant id_to_person_info_dict attribute, now we only have phone_to_person_info_dict attribute that is the mapping for the people sheet containing all necessary information
    - naming notation: mappings -> mapping
    
    Signed-off-by: CuriousGeorgiy <32464072+curiousgeorgiy@users.noreply.github.com>

--- server/telegram/telegram_api.py
+++ server/telegram/telegram_api.py
@@ -11,7 +11,7 @@ import util.file_processing
 class TelegramAPI:
 
     def __init__(self, people_sheet, push_notifications_sheet, faq_sheet, classes_schedule_sheet, config,
-                 user_data_json_path, use_user_data_json=True, use_proxy=False):
+                 user_data_json_path, use_user_data_json=True, use_proxy=True):
         self._persistence_path = user_data_json_path
         self._persistence = telegram.ext.DictPersistence(store_chat_data=True,
                                                          user_data_json=util.file_processing.load_json_string_from_file(
@@ -22,7 +22,7 @@ class TelegramAPI:
                                              persistence=self._persistence) if use_proxy \
             else telegram.ext.Updater(token=config['bot_token'], use_context=True, persistence=self._persistence)
 
-        self._create_mappings_from_people_sheet(people_sheet)
+        self._create_mapping_from_people_sheet(people_sheet)
         self._push_notifications_sheet = push_notifications_sheet
         self._faq_sheet = faq_sheet
         self._classes_schedule_sheet = classes_schedule_sheet
@@ -56,21 +56,19 @@ class TelegramAPI:
     def _create_update_data_job(self):
         self._updater.job_queue.run_repeating(job_callbacks.update_data_sheets, datetime.timedelta(seconds=30))
 
-    def _create_mappings_from_people_sheet(self, people_sheet):
-        self._phone_number_to_id_dict = {}
-        self._id_to_person_info_dict = {}
+    def _create_mapping_from_people_sheet(self, people_sheet):
+        self._phone_number_to_person_info_dict = {}
 
         for i in range(1, len(people_sheet)):
-            self._phone_number_to_id_dict[people_sheet[i][0]] = i
-            self._id_to_person_info_dict[i] = {'name': people_sheet[i][1], 'groups': set()}
+            self._phone_number_to_person_info_dict[people_sheet[i][0]] = {'name': people_sheet[i][1], 'groups': set()}
             for j in range(2, len(people_sheet[0])):
                 if int(people_sheet[i][j]):
-                    self._id_to_person_info_dict[i]['groups'].add(people_sheet[0][j])
+                    self._phone_number_to_person_info_dict[people_sheet[i][0]]['groups'].add(people_sheet[0][j])
 
         for telegram_id, user_data in self._updater.dispatcher.user_data.items():
             try:
                 if user_data['authorized']:
-                    self._id_to_person_info_dict[user_data['id_in_telegram_api']]['telegram_id'] = telegram_id
+                    self._phone_number_to_person_info_dict[user_data['phone_number']]['telegram_id'] = telegram_id
             except KeyError:
                 pass
 
@@ -82,7 +80,7 @@ class TelegramAPI:
             if send_time.timestamp() > datetime.datetime.today().timestamp():
                 self._push_notification_jobs.append(self._updater.job_queue.run_once(
                     job_callbacks.create_push_notification_callback_from_push_text(text), send_time,
-                    context=(self._id_to_person_info_dict, groups)))
+                    context=(self._phone_number_to_person_info_dict, groups)))
 
     def _create_faq_handlers(self):
         for question, answer in self._faq_sheet:
@@ -103,9 +101,8 @@ class TelegramAPI:
 
     """ Remove methods """
 
-    def _remove_mappings_for_people_sheet(self):
-        self._phone_number_to_id_dict.clear()
-        self._id_to_person_info_dict.clear()
+    def _remove_mapping_for_people_sheet(self):
+        self._phone_number_to_person_info_dict.clear()
 
     def _remove_prev_push_notification_jobs(self, prev_push_notification_jobs_sheet_length):
         push_notification_jobs_to_be_deleted = self._push_notification_jobs[:prev_push_notification_jobs_sheet_length]
@@ -122,9 +119,9 @@ class TelegramAPI:
 
     """ Update methods """
 
-    def _update_mappings_for_people_sheet(self, people_sheet):
-        self._remove_mappings_for_people_sheet()
-        self._create_mappings_from_people_sheet(people_sheet)
+    def _update_mapping_for_people_sheet(self, people_sheet):
+        self._remove_mapping_for_people_sheet()
+        self._create_mapping_from_people_sheet(people_sheet)
 
     def _update_push_notification_jobs(self):
         push_notification_jobs_sheet_length = len(self._push_notification_jobs)
@@ -137,7 +134,7 @@ class TelegramAPI:
         self._remove_prev_faq_handlers(faq_handlers_sheet_length)
 
     def update_data_sheets(self, people_sheet, push_notifications_sheet, faq_sheet, classes_schedule_sheet):
-        self._update_mappings_for_people_sheet(people_sheet)
+        self._update_mapping_for_people_sheet(people_sheet)
 
         self._push_notifications_sheet = push_notifications_sheet
         self._update_push_notification_jobs()
@@ -149,13 +146,13 @@ class TelegramAPI:
 
     """ Set methods """
 
-    def set_person_telegram_id(self, person_id, telegram_id):
-        self._id_to_person_info_dict[person_id]['telegram_id'] = telegram_id
+    def set_person_telegram_id(self, phone_number, telegram_id):
+        self._phone_number_to_person_info_dict[phone_number]['telegram_id'] = telegram_id
 
     """ Get methods """
 
-    def get_mappings_for_people_sheet(self):
-        return self._phone_number_to_id_dict, self._id_to_person_info_dict
+    def get_mapping_for_people_sheet(self):
+        return self._phone_number_to_person_info_dict
 
     def get_classes_schedule_sheet(self):
         return self._classes_schedule_sheet
