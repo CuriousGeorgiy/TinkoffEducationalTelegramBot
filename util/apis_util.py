80441b1f40791b183d38b615e7f197c59bab7915
CuriousGeorgiy <32464072+curiousgeorgiy@users.noreply.github.com>
2019-07-26 01:50:16 +0300

    [telegram_util.py -> apis_util.py]
    moved and renamed file: TelegramUtil -> APIsUtil
    
    implemented:
    - get_telegram_api_classes_schedule_sheet static method
    
    changes:
    - added assert statements in all functions that manipulate the api objects
    
    Signed-off-by: CuriousGeorgiy <32464072+curiousgeorgiy@users.noreply.github.com>

---
+++ util/apis_util.py
@@ -0,0 +1,47 @@
+from server.telegram.telegram_api import TelegramAPI
+from resources.google_sheets_api import GoogleSheetsAPI
+
+
+class APIsUtil:
+    _telegram_api = None
+    _google_sheets_api = None
+
+    @staticmethod
+    def attach_telegram_api(telegram_api: TelegramAPI):
+        APIsUtil._telegram_api = telegram_api
+
+    @staticmethod
+    def attach_google_sheets_api(google_sheets_api: GoogleSheetsAPI):
+        APIsUtil._google_sheets_api = google_sheets_api
+
+    @staticmethod
+    def dump_telegram_api_persistence_obj():
+        assert APIsUtil._telegram_api
+
+        APIsUtil._telegram_api.dump_persistence_obj()
+
+    @staticmethod
+    def update_telegram_api_data_sheets():
+        assert APIsUtil._telegram_api and APIsUtil._google_sheets_api
+
+        APIsUtil._telegram_api.update_data_sheets(**APIsUtil._google_sheets_api.extract_all_sheets())
+
+    @staticmethod
+    def set_telegram_api_person_telegram_id(person_id, telegram_id):
+        assert APIsUtil._telegram_api
+
+        APIsUtil._telegram_api.set_person_telegram_id(person_id, telegram_id)
+
+    """ Get methods """
+
+    @staticmethod
+    def get_telegram_api_mappings_for_people_sheet():
+        assert APIsUtil._telegram_api
+
+        return APIsUtil._telegram_api.get_mappings_for_people_sheet()
+
+    @staticmethod
+    def get_telegram_api_classes_schedule_sheet():
+        assert APIsUtil._telegram_api
+
+        return APIsUtil._telegram_api.get_classes_schedule_sheet()
