c478c16d96164d3b13f5ca2afd30ace93232e6d0
CuriousGeorgiy <32464072+curiousgeorgiy@users.noreply.github.com>
2019-07-26 02:26:23 +0300

    [job_callbacks.py]
    changes:
    
    - name notation: person -> person_info
    
    Signed-off-by: CuriousGeorgiy <32464072+curiousgeorgiy@users.noreply.github.com>

--- server/telegram/callbacks/job_callbacks.py
+++ server/telegram/callbacks/job_callbacks.py
@@ -5,10 +5,10 @@ import telegram.ext
 def create_push_notification_callback_from_push_text(push_text):
     @telegram.ext.dispatcher.run_async
     def callback(context):
-        for person in context.job.context[0].values():
-            if person['groups'] & context.job.context[1]:
+        for person_info in context.job.context[0].values():
+            if person_info['groups'] & context.job.context[1]:
                 try:
-                    context.bot.send_message(chat_id=person['telegram_id'], text=push_text)
+                    context.bot.send_message(chat_id=person_info['telegram_id'], text=push_text)
                 except KeyError:
                     pass
 
