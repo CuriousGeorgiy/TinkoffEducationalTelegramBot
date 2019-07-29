7304f11ae44d8a05911b3821f635089cc2d980e4
CuriousGeorgiy <32464072+curiousgeorgiy@users.noreply.github.com>
2019-07-30 02:43:18 +0300

    [main.py]
    renamed: google_sheets_ids -> google_sheets_config
    
    refactoring:
    - simplified construction of telegram_api
    
    Signed-off-by: CuriousGeorgiy <32464072+curiousgeorgiy@users.noreply.github.com>

--- main.py
+++ main.py
@@ -13,35 +13,26 @@ def create_argument_parser():
     argument_parser.add_argument('--proxy', dest='proxy', action='store_true', help='use proxy for the telegram api')
     argument_parser.add_argument('--no-user-data-json', dest='use_user_data_json', action='store_false',
                                  help='by setting this option you won\'t use the existing user data json and will'
-                                      'overwrite it')
+                                      ' overwrite it')
     argument_parser.set_defaults(proxy=False, use_user_data_json=True)
 
     return argument_parser
 
 
 def main(proxy, use_user_data_json):
-    google_sheets_ids = util.file_processing.load_json_from_file('misc/google_sheets_ids.json')
+    google_sheets_config = util.file_processing.load_json_from_file('misc/google_sheets_config.json')
     logging.config.dictConfig(util.file_processing.load_json_from_file('misc/logging_config.json'))
     telegram_api_config = util.file_processing.load_json_from_file('misc/telegram_api_config.json')
 
-    google_sheets_api = GoogleSheetsAPI(google_sheets_ids)
+    google_sheets_api = GoogleSheetsAPI(google_sheets_config)
 
     if not os.path.exists('misc/telegram_api_user_data.json'):
         with open('misc/telegram_api_user_data.json', 'w') as file:
             file.write('{}')
 
-    if use_user_data_json:
-        telegram_api = TelegramAPI(**google_sheets_api.extract_all_sheets(), config=telegram_api_config,
-                                   user_data_json_path='misc/telegram_api_user_data.json',
-                                   use_user_data_json=True, use_proxy=True) if proxy \
-            else TelegramAPI(**google_sheets_api.extract_all_sheets(), config=telegram_api_config,
-                             user_data_json_path='misc/telegram_api_user_data.json')
-    else:
-        telegram_api = TelegramAPI(**google_sheets_api.extract_all_sheets(), config=telegram_api_config,
-                                   user_data_json_path='misc/telegram_api_user_data.json',
-                                   use_user_data_json=False, use_proxy=True) if proxy \
-            else TelegramAPI(**google_sheets_api.extract_all_sheets(), config=telegram_api_config,
-                             user_data_json_path='misc/telegram_api_user_data.json', use_user_data_json=False)
+    telegram_api = TelegramAPI(**google_sheets_api.extract_all_sheets(), config=telegram_api_config,
+                               user_data_json_path='misc/telegram_api_user_data.json',
+                               use_user_data_json=use_user_data_json, use_proxy=proxy)
 
     APIsUtil.attach_telegram_api(telegram_api)
     APIsUtil.attach_google_sheets_api(google_sheets_api)
