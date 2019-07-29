740c42bfa68ca51e641295b56b5bd7c657c6b7b8
CuriousGeorgiy <32464072+curiousgeorgiy@users.noreply.github.com>
2019-07-30 02:41:09 +0300

    [message_callbacks.py]
    changes:
    - authorization callback function checks person's authorization status
    and in case he's not in the google sheets, offers to add him there. it returns the next state of the conversation handler
    
    implemented:
    - callback functions for the conversation handler: request_continuation_of_conversation, ask_for_name, end_conversation, push_personal_info_to_people_sheet
    
    Signed-off-by: CuriousGeorgiy <32464072+curiousgeorgiy@users.noreply.github.com>

--- telegram_/callbacks/message_callbacks.py
+++ telegram_/callbacks/message_callbacks.py
@@ -24,20 +24,65 @@ def authorization(update, context):
     if update.message.contact.phone_number[0].isdigit():
         update.message.contact.phone_number = '+' + update.message.contact.phone_number
 
-    if update.message.contact.phone_number in phone_number_to_person_info_dict:
+    context.user_data['phone_number'] = update.message.contact.phone_number
+
+    if context.user_data['phone_number'] in phone_number_to_person_info_dict and \
+            phone_number_to_person_info_dict[context.user_data['phone_number']]['authorization_status'] == '1':
+
         context.user_data['authorized'] = True
-        context.user_data['phone_number'] = update.message.contact.phone_number
         APIsUtil.set_telegram_api_person_telegram_id(update.message.contact.phone_number, update.message.chat_id)
 
         context.bot.send_message(chat_id=update.message.chat_id,
-                                 text='Вы прошли авторизацию можете пользоваться всеми функциями бота.',
-                                 reply_markup=telegram.ReplyKeyboardRemove())
+                                 text='Вы прошли авторизацию и теперь можете пользоваться всеми функциями бота.')
 
         APIsUtil.dump_telegram_api_persistence_obj()
     else:
-        context.bot.send_message(chat_id=update.message.chat_id,
-                                 text='Сожалею, но Вы не прошли авторизацию.',
-                                 reply_markup=telegram.ReplyKeyboardRemove())
+        context.bot.send_message(chat_id=update.message.chat_id, text='Сожалею, но Вы не прошли авторизацию.')
+        if context.user_data['phone_number'] in phone_number_to_person_info_dict:
+            context.bot.send_message(chat_id=update.message.chat_id, text='Вы уже есть в моей ведомости. В случае'
+                                                                          ' успешного прохождения отбора я Вас'
+                                                                          ' авторизую.')
+        if not (context.user_data['phone_number'] in phone_number_to_person_info_dict):
+            context.bot.send_message(chat_id=update.message.chat_id, text='Если Вы хотите ходить на курсы Tinkoff'
+                                                                          ' Generation, я могу добавить Вас в свою'
+                                                                          ' ведомость и в случае успешного прохождения'
+                                                                          ' отбора авторизовать.',
+                                     reply_markup=telegram.ReplyKeyboardMarkup([['Хочу', 'Не хочу']],
+                                                                               one_time_keyboard=True))
+
+            return 'pending_answer'
+
+
+@telegram.ext.dispatcher.run_async
+@telegram_.decorators.send_action(telegram.ChatAction.TYPING)
+def request_continuation_of_conversation(update, context):
+    context.bot.send_message(chat_id=update.message.chat_id, text='Требуется закончить диалог.')
+
+
+@telegram.ext.dispatcher.run_async
+@telegram_.decorators.send_action(telegram.ChatAction.TYPING)
+def ask_for_name(update, context):
+    context.bot.send_message(chat_id=update.message.chat_id, text='Как Вас зовут (имя фамилия)?')
+
+    return 'pending_name'
+
+
+@telegram.ext.dispatcher.run_async
+@telegram_.decorators.send_action(telegram.ChatAction.TYPING)
+def end_conversation(update, context):
+    return telegram.ext.ConversationHandler.END
+
+
+@telegram.ext.dispatcher.run_async
+@telegram_.decorators.send_action(telegram.ChatAction.TYPING)
+def push_personal_info_to_people_sheet(update, context):
+    from util.apis_util import APIsUtil
+
+    APIsUtil.append_person_to_people_sheet('\'' + context.user_data['phone_number'], update.message.text)
+
+    context.bot.send_message(chat_id=update.message.chat_id, text='Отлично, я добавил Вас в свою ведомость.')
+
+    return telegram.ext.ConversationHandler.END
 
 
 @telegram.ext.dispatcher.run_async  # May cause problems, needs to be tested.
@@ -55,7 +100,7 @@ def nearest_class(update, context):
 
             for group, class_type, class_date in classes_schedule:
                 if (group in groups) and (class_date.timestamp() > datetime.datetime.today().timestamp()):
-                    message_text += 'Ближайшее по расписанию занятие у направления {0} {1}. Тип занятия: {2}.\n'.\
+                    message_text += 'Ближайшее по расписанию занятие у направления {0} {1} . Тип занятия: {2}.\n'.\
                         format(group, class_date.strftime('%d.%m.%Y %H:%M'), class_type)
                     groups.remove(group)
 
@@ -64,7 +109,7 @@ def nearest_class(update, context):
     except KeyError:
         context.bot.send_message(chat_id=update.message.chat_id,
                                  text='Для использования некоторых фукнций бота необходимо пройти авторизацию с помощью'
-                                      ' команды /authorization.')
+                                      ' команды /authorization .')
 
 
 personalized_callbacks_dict = {'nearest_class': nearest_class}
